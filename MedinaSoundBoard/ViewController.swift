3830bf27fb46caa2e8d87d1b28449fc377f14dce
joaquin-1204 <joaquin.salas@tecsup.edu.pe>
2023-05-24 08:17:59 -0500

    Initial Commit

---
+++ MedinaSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedinaSoundBoard
+//
+//  Created by Mac 08 on 24/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
